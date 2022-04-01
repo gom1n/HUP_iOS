d59d455411dc2d4f387babda5fafc3072b4a9ecc
gom1n <alice436777@gmail.com>
2022-04-01 23:55:15 +0900

    Initial Commit

---
+++ HUP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HUP
+//
+//  Created by gomin on 2022/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
