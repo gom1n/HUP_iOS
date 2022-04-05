130bf74e1af49fcf8b45825ee0c17be70dd2a555
gom1n <alice436777@gmail.com>
2022-04-05 23:52:35 +0900

    [feat] Add register page

---
+++ HUP/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  HUP
+//
+//  Created by gomin on 2022/04/02.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func registerButtonDidTap(_ sender: UIButton) {
+        //화면전환
+        //1 스토리 보드를 생성
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //2 뷰컨트롤러를 생성
+        let registerViewController = storyboard.instantiateViewController(withIdentifier: "registerVC") as! RegisterViewController
+        //3 화면전환 메소드를 사용해서 화면을 전환
+//        self.present(registerViewController, animated: true, completion: nil)
+        self.navigationController?.pushViewController(registerViewController, animated: true)
+    }
+}
