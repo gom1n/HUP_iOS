9196e186c8e7dc5b34aa0294002e53ba2fef3ed9
gom1n <alice436777@gmail.com>
2022-05-26 21:00:57 +0900

    [style] Set email check page buttons

--- HUP/Authentication/Login/LoginDataManager.swift
+++ HUP/Authentication/Login/LoginDataManager.swift
@@ -39,23 +39,4 @@ class LoginDataManager {
             }
         }
     }
-    // GOOGLE LOGIN
-    func googleLoginDataManager(_ parameter : GoogleLoginInput) {
-        AF.request("http://www.hurryuphup.me/api/v1/users/google-login",
-                   method: .post,
-                   parameters: parameter,
-                   encoder: JSONParameterEncoder.default,
-                   headers: nil)
-            .validate()
-            .responseDecodable(of: GoogleLoginModel.self) { response in
-                
-            switch response.result {
-            case .success(let result):
-                print("gLogin: ", result)
-//                tableViewCell.bestItemSuccessAPI(result)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
 }
