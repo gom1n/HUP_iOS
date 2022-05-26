405ceb977564d1bde3feeeb61d045b6ca5cc4867
gom1n <alice436777@gmail.com>
2022-05-26 19:08:14 +0900

    [chore] Fix function error and Set valid functions on textfields

--- HUP/Authentication/Register/RegisterViewController.swift
+++ HUP/Authentication/Register/RegisterViewController.swift
@@ -8,62 +8,101 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    // textfields
     @IBOutlet private weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var pwCheckTextField: UITextField!
     @IBOutlet weak var nickNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    // buttons
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var idCheckButton: UIButton!
+    // attributes
+    var id: String?
+    var pw: String?
+    var nickname: String?
+    var email: String?
     
-    var isIdChecked: Bool? = false
+    var isValidEmail = false
+    var isValidId = false
+    var isValidNickname = false
+    var isValidPassword = false
+    var isValidPasswordCheck = false
+    var isIdChecked = false
+    
+    var textFields: [UITextField] {
+        [idTextField, pwTextField, nickNameTextField, emailTextField]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        isValidTf()
         self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
     }
-    // MARK: - Actions
+    // check is valid text fields and change button state
+    func isValidTf() {
+        if isValidEmail && isValidId && isValidNickname && isValidPassword && isValidPasswordCheck && isIdChecked {
+            self.registerButton.backgroundColor = UIColor(named: "HupColor")
+            self.registerButton.isEnabled = true
+        } else {
+            self.registerButton.backgroundColor = UIColor(named: "disabledButtonColor")
+            self.registerButton.isEnabled = false
+        }
+        if isValidId {
+            self.idCheckButton.backgroundColor = UIColor(named: "HupColor")
+            self.idCheckButton.isEnabled = true
+        } else {
+            self.idCheckButton.backgroundColor = UIColor(named: "disabledButtonColor")
+            self.idCheckButton.isEnabled = false
+        }
+    }
+    // MARK: - Actions - textFields editing changed
+    @IBAction func idTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.isValidId = text.isValidId()
+        self.id = text
+        isValidTf()
+        print("isValidId ", self.isValidId)
+    }
+    @IBAction func pwTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.isValidPassword = text.isValidPassword()
+        self.pw = text
+        isValidTf()
+        print("isValidPassword ", self.isValidPassword)
+    }
+    @IBAction func pwCheckTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        isValidPasswordCheck = (text == self.pw) ? true : false
+        print("isValidPWCHeck ", self.isValidPasswordCheck)
+        isValidTf()
+    }
+    @IBAction func nickNameTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.isValidNickname = text.isValidNickName()
+        self.nickname = text
+        isValidTf()
+        print("isValidNickname ", self.isValidNickname)
+    }
+    @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.isValidEmail = text.isValidEmail()
+        self.email = text
+        isValidTf()
+        print("isValidEmail ", self.isValidEmail)
+    }
+    // MARK: - Actions - button tap
+    // id check button click
     @IBAction func idCheckButtonTap(_ sender: Any) {
-        let loginId = idTextField.text!
+        let loginId = (self.id)!
+        print("edited loginId: ", loginId)
         RegisterDataManager().idCheckDataManager(loginId, self)
     }
-    // MARK: 회원가입 버튼 클릭
+    // 회원가입 버튼 클릭
     @IBAction func registerButtonTap(_ sender: UIButton) {
-        var id = idTextField.text!
-        var pw = pwTextField.text!
-        var pwCheck = pwCheckTextField.text!
-        var nickname = nickNameTextField.text!
-        var email = emailTextField.text!
-        // MARK: id text field
-        if !(id.count >= 5 && id.count <= 10) {
-            UIManager().showToast(message: "아이디는 최소 5글자 이상 10글자 이하여야 합니다.", viewController: self)
-            return
-        }
-        // MARK: id check
-        if !(isIdChecked!) {
-            UIManager().showToast(message: "아이디 중복확인을 해주세요.", viewController: self)
-            return
-        }
-        // MARK: password text field
-        if !((pw.isValidPassword())) {
-            UIManager().showToast(message: "비밀번호는 영어, 숫자, 특수문자를 조합해 8~12자리로 입력해주세요.", viewController: self)
-            return
-        }
-        // MARK: password check
-        if (pw != pwCheck) {
-            UIManager().showToast(message: "비밀번호가 서로 일치하지 않습니다.", viewController: self)
-            return
-        }
-        // MARK: username text field
-        if !(nickname.count >= 2 && nickname.count <= 10) {
-            UIManager().showToast(message: "이름은 최소 2글자 이상 10글자 이하여야 합니다.", viewController: self)
-            return
-        }
-        // MARK: email text field
-        if !(email.isValidEmail()) {
-            UIManager().showToast(message: "이메일을 알맞은 형태로 입력해주세요.", viewController: self)
-            return
-        }
         // MARK: register
-        let registerInput = RegisterInput(email: email, loginId: id, password: pw, phoneNumber: nil, username: nickname)
+        let registerInput = RegisterInput(email: self.email, loginId: self.id, password: self.pw, phoneNumber: nil, username: self.nickname)
         RegisterDataManager().registerDataManager(registerInput, self)
     }
     
@@ -74,12 +113,7 @@ class RegisterViewController: UIViewController {
 extension RegisterViewController {
     func idCheckSuccessAPI(_ result: IdCheckModel) {
         UIManager().showToast(message: result.message!, viewController: self)
-        let message = result.message!
-        if message == "사용가능한 이메일입니다." {
-            self.isIdChecked = true
-        } else {
-            self.isIdChecked = false
-        }
+        self.isIdChecked = true
     }
     func registerSuccessAPI(_ result: RegisterModel) {
         UIManager().showToast(message: result.message!, viewController: self)
@@ -96,7 +130,7 @@ extension String {
     }
     // 비밀번호는 영어, 숫자, 특수문자를 조합해 8~12자리로 입력해주세요.
     func isValidPassword() -> Bool {
-        let passwordRegEx = "^[a-z0-9_-]{8,12}$"
+        let passwordRegEx = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,12}"
         let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
         return passwordTest.evaluate(with: self)
     }
