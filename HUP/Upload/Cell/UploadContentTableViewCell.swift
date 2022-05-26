cec6d6f15f44952038bbf5e85a5209ce611bd3d9
gom1n <alice436777@gmail.com>
2022-05-27 06:49:49 +0900

    [feat] Add item status star rating

--- HUP/Upload/Cell/UploadContentTableViewCell.swift
+++ HUP/Upload/Cell/UploadContentTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Cosmos
 
 class UploadContentTableViewCell: UITableViewCell {
     @IBOutlet weak var itemNameText: UITextField!
@@ -13,22 +14,31 @@ class UploadContentTableViewCell: UITableViewCell {
     @IBOutlet weak var itemPriceText: UITextField!
     @IBOutlet weak var itemBuyPriceText: UITextField!
     @IBOutlet weak var itemEndDateText: UITextField!
-    @IBOutlet weak var itemContentText: UITextView!
+    @IBOutlet weak var itemDescriptionText: UITextView!
     @IBOutlet weak var itemEndTimeText: UITextField!
+    @IBOutlet weak var itemStatusView: CosmosView!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        itemContentText.layer.borderColor = UIColor.systemGray5.cgColor
-        itemContentText.layer.borderWidth = 1.0
+        itemDescriptionText.layer.borderColor = UIColor.systemGray5.cgColor
+        itemDescriptionText.layer.borderWidth = 1.0
+        
+        // 별점 set
+        itemStatusView.rating = 1
+        itemStatusView.settings.updateOnTouch = true
+        itemStatusView.settings.fillMode = .full
+        itemStatusView.settings.starSize = 30
+        itemStatusView.settings.starMargin = 5
+        itemStatusView.settings.filledColor = UIColor(named: "HupColor") ?? UIColor.gray
+        itemStatusView.settings.emptyBorderColor = UIColor(named: "HupColor") ?? UIColor.gray
+        itemStatusView.settings.filledBorderColor = UIColor(named: "HupColor") ?? UIColor.gray
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-    }
-    func setItemData() {
-        
     }
 }
